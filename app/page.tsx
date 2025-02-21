861b2e980941c9b7010fee19da14ba182c3a46f5
minseochh02 <minseochh02@gmail.com>
2025-02-21 16:19:45 +0900

    claude suggest

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,9 @@ import styles from "./test.module.css";
 
 export default function Home() {
 	return (
-		<div className={styles.iframeContainer}>
+		<div>
 			<h1>Test Page</h1>
-			<iframe
-				className={styles.iframe}
-				src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQsPgphmRJq61JPI8DV_R6wUjHjoG8x_mRjU4NVI2Bk-6zx94ZPXV4XTH4lPRZK8j0k1iFKuIZEOHZr/pubhtml?gid=421427572&widget=true&headers=false&chrome=false&rm=minimal"
-			></iframe>
+			<iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQsPgphmRJq61JPI8DV_R6wUjHjoG8x_mRjU4NVI2Bk-6zx94ZPXV4XTH4lPRZK8j0k1iFKuIZEOHZr/pubhtml?gid=421427572&widget=true&headers=false&chrome=false&rm=minimal"></iframe>
 		</div>
 	);
 }
