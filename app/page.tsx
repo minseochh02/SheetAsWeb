02da7c5c0101ac5e2c0c5186ce0952e79dff584c
minseochh02 <minseochh02@gmail.com>
2025-02-21 16:30:55 +0900

    claude suggest

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,10 @@ export default function Home() {
 	return (
 		<div>
 			<h1>Test Page</h1>
-			<iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQsPgphmRJq61JPI8DV_R6wUjHjoG8x_mRjU4NVI2Bk-6zx94ZPXV4XTH4lPRZK8j0k1iFKuIZEOHZr/pubhtml?gid=421427572&widget=true&headers=false&chrome=false&rm=minimal"></iframe>
+			<iframe
+				src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQsPgphmRJq61JPI8DV_R6wUjHjoG8x_mRjU4NVI2Bk-6zx94ZPXV4XTH4lPRZK8j0k1iFKuIZEOHZr/pubhtml?gid=421427572&widget=true&headers=false&chrome=false&rm=minimal"
+				className={styles.iframe}
+			></iframe>
 		</div>
 	);
 }
