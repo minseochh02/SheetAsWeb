91fbcea4fbbe0c4509b6f4057207e908b12ae0ee
minseochh02 <minseochh02@gmail.com>
2025-02-21 15:58:45 +0900

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import styles from "./test.module.css";
+
+export default function Home() {
+	return (
+		<div className={styles.iframeContainer}>
+			<h1>Test Page</h1>
+			<iframe
+				className={styles.iframe}
+				src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQsPgphmRJq61JPI8DV_R6wUjHjoG8x_mRjU4NVI2Bk-6zx94ZPXV4XTH4lPRZK8j0k1iFKuIZEOHZr/pubhtml?gid=421427572&amp;single=true&amp;widget=true&amp;headers=false"
+			></iframe>
+		</div>
+	);
+}
