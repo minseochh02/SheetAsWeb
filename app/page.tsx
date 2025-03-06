47aa9ab1014023cf84f9dc502b295468aa0e58d5
minseochh02 <minseochh02@gmail.com>
2025-03-06 10:05:22 +0900

    new link

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import IframeLoader from "./components/IframeLoader";
 export default function Home() {
 	return (
 		<div>
-			<IframeLoader src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQsPgphmRJq61JPI8DV_R6wUjHjoG8x_mRjU4NVI2Bk-6zx94ZPXV4XTH4lPRZK8j0k1iFKuIZEOHZr/pubhtml?gid=421427572&amp;single=true&amp;widget=true&amp;headers=false" />
+			<IframeLoader src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQyLz3zjbdeRfBOFMJXEB--vbZeVap_IEmffXcXIq8XGtnOYxtEpnlP0l-MZ9NaPC4Z_8J1ZaADyH7B/pubhtml?gid=421427572&amp;single=true&amp;widget=true&amp;headers=false" />
 		</div>
 	);
 }
